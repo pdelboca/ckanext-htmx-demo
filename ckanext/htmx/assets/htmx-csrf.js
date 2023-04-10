8f1187f84c5b1ffe8591ac8b6bd8da01f4516544
pdelboca <patriciodelboca@gmail.com>
2023-04-10 10:36:25 -0300

    Get CSRFToken field name

--- ckanext/htmx/assets/htmx-csrf.js
+++ ckanext/htmx/assets/htmx-csrf.js
@@ -1,7 +1,12 @@
-const csrfToken = $('meta[name=_csrf_token]').attr('content')
+/**
+ * This script is used to add the CSRF token to the HTMX request headers.
+ * 
+ */
+var csrf_field = $('meta[name=csrf_field_name]').attr('content');
+var csrf_token = $('meta[name='+ csrf_field +']').attr('content');
 
 htmx.on('htmx:configRequest', (event) => {
-  if (csrfToken) {
-    event.detail.headers['x-csrftoken'] = csrfToken;
+  if (csrf_token) {
+    event.detail.headers['x-csrftoken'] = csrf_token;
   }
 });
