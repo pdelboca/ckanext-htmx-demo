242b78d9cd0c0d0f00f068849e167b2053894923
pdelboca <patriciodelboca@gmail.com>
2023-04-10 10:44:24 -0300

    Add docstring to helpers

--- ckanext/htmx/helpers.py
+++ ckanext/htmx/helpers.py
@@ -1,6 +1,11 @@
 import re
 
 def add_param_or_remove_it_if_exist(url, key, value):
+    """Add a parameter to the URL or remove it if it already exists.
+    
+    It is used to htmx_facet_list.html to help with the URL attribute
+    of the selected facet.
+    """
     value = value.replace(" ", "%20")
     # regex to match key=value or &key=value
     pattern = f"([&?]){key}={value}"
@@ -18,7 +23,15 @@ def add_param_or_remove_it_if_exist(url, key, value):
 
 
 def remove_param_if_exist(url, key, value):
-    """Remove a parameter from the URL if it exists."""
+    """Remove a parameter from the URL if it exists.
+    
+    This is user in htmx_search.html to remove the `q` and `sort` parameter from
+    the URL. For some reason, htmx does not remove the `q` parameter from the
+    URL when a new search is done, causing the `&q=search_term` to be appended 
+    each time a user does a query.
+
+    This may be a bug in htmx, but I am not sure.
+    """
     value = value.replace(" ", "%20")
     pattern = f"([&?]){key}={value}"
     match = re.search(pattern, url)
